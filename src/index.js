f3514e3deee89ad1175a9319ba6b64ce9ef401a1
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-05-28 18:05:02 +0300

    ADD: redux logic

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-
+// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/eliftechDelivery">
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/eliftechDelivery">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </React.StrictMode>
 );
 
---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  `auth/register`,
+  async ({ name, email, password }, thunkApi) => {
+    try {
+      const response = await axios.post('/users/signup', {
+        name,
+        email,
+        password,
+      });
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (e) {
+      Notiflix.Notify.failure('Bad Request! Try more corresponding password!');
+      return thunkApi.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  `auth/login`,
+  async ({ email, password }, thunkApi) => {
+    try {
+      const response = await axios.post('/users/login', {
+        email,
+        password,
+      });
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (e) {
+      Notiflix.Notify.failure('Bad Request! Try It again!');
+      return thunkApi.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(`auth/logout`, async (_, thunkApi) => {
+  try {
+    const response = await axios.post('/users/logout');
+    clearAuthHeader();
+    return response.data;
+  } catch (e) {
+    Notiflix.Notify.failure('Bad Request! Try It again!');
+    return thunkApi.rejectWithValue(e.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  `auth/refreshUser`,
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Can not find user');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (error) {
+      Notiflix.Notify.failure('Bad Request! Try It again!');
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
