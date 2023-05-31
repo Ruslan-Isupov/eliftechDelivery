ff90d6cdf881ff6ba1d2e9d2d7557daad146e92e
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-05-31 11:03:01 +0300

    CHANGE: eventListener for input in CartItem

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,49 +1,100 @@
-import css from "./CartItem.module.css"
+import css from "./CartItem.module.css";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { selectCart } from "../../redux/selectors";
 import { deleteGood } from "../../redux/cart/cartSlice";
-import { useState } from 'react';
-import food from '../../images/food.jpg'
+import { useState } from "react";
+import food from "../../images/food.jpg";
+
 
 export const CartItem = ({ name, price, id, image }) => {
-
-  // let counter = "";
-    const LOCALSTORAGE_KEY = id;
-  const getAmount = localStorage.getItem(LOCALSTORAGE_KEY)
-  
-  // console.log(getAmount)
-    // const currentQuantity = localStorage.getItem(LOCALSTORAGE_KEY)
-  const { quantity } = JSON.parse(getAmount)
-  const [valueQuantity,setvalueQuantity] = useState(quantity)
-  const updateQuantity = (e) => {
-   
-    setvalueQuantity(e.target.value)
-    
-  }
-
-  
+   let totalPrice = 0;
   const { products } = useSelector(selectCart);
   const dispatch = useDispatch();
+  const LOCALSTORAGE_KEY = id;
+  const infoDish = localStorage.getItem(LOCALSTORAGE_KEY);
+  // if (getAmount !== null) {
+  //   const { quantity } = JSON.parse(getAmount)
+  //    const [valueQuantity,setvalueQuantity] = useState(quantity)
+  // }
+
+  const { quantity } = JSON.parse(infoDish ? infoDish : 1);
   
+  const [valueQuantity, setvalueQuantity] = useState(quantity);
+    const changeQuantity = (e) => {
+    setvalueQuantity(e.target.value);
+    // console.log(valueQuantity);
+    // console.log(e.target.value);
+    // localStorage.setItem(
+    //   LOCALSTORAGE_KEY,
+    //   JSON.stringify({
+    //     id,
+    //      quantity: valueQuantity,
+    //     // quantity: Number(valueQuantity)+1,
+    //     sumPrice : quantity*price
+    //   })
+    // );
+  };
+   localStorage.setItem(
+      LOCALSTORAGE_KEY,
+      JSON.stringify({
+        id,
+         quantity: valueQuantity,
+        // quantity: Number(valueQuantity)+1,
+        sumPrice : quantity*price
+      })
+    );
+
+
+
+  const { sumPrice } = JSON.parse(infoDish ? infoDish : 1);
+  totalPrice += sumPrice
+  console.log(totalPrice)
+    // const { price } = products.find((product) => product.id === id)
+ 
+
+//   const countTotalPrice = (id) => {
+//     // const { price } = products.find((product) => product.id === id)
+//     // console.log(price * valueQuantity)
+//     // const {quantity}  = JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY'));
+// }
+
   const deleteDish = (id) => {
     const deletedGood = products.find((product) => product.id === id);
     dispatch(deleteGood(deletedGood));
   };
-    return (
- <>
-      <img className={css.cartImage} src={food} alt="" width="200" height="150" />
-        <div className={css.cartBox}>
-          <div className={css.infoCart}>  
-            <p className={css.cartName}> {name}</p>
-            <p className={css.cartPrice}>{price}</p>
-              {/* <input type="number"  step="1" min="1" max="100" id="num_count" name="quantity" pattern="[0-9]*" inputMode="numeric" width="10" height="10"/> */}
-            <input type="number" onChange={updateQuantity} step="1" min="1" max="100" id="num_count" name="quantity" value={valueQuantity} pattern="[0-9]*" inputMode="numeric" width="10" height="10"/>
-       </div>
-        <button className={css.cartButton} onClick={()=>deleteDish(id)}>
+  return (
+    <>
+      <img
+        className={css.cartImage}
+        src={food}
+        alt="dish"
+        width="200"
+        height="150"
+      />
+      <div className={css.cartBox}>
+        <div className={css.infoCart}>
+          <p className={css.cartName}> {name}</p>
+          <p className={css.cartPrice}>{price}</p>
+          <input
+            type="number"
+            onChange={changeQuantity}
+            step="1"
+            min="1"
+            max="100"
+            id="num_count"
+            name="quantity"
+            value={valueQuantity}
+            pattern="[0-9]*"
+            inputMode="numeric"
+            width="10"
+            height="10"
+          />
+        </div>
+        <button className={css.cartButton} onClick={() => deleteDish(id)}>
           Delete
         </button>
       </div>
     </>
-    )
-}
+  );
+};
--- src/components/GoodsItem/GoodsItem.jsx
+++ src/components/GoodsItem/GoodsItem.jsx
@@ -3,55 +3,45 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addGood } from "../../redux/cart/cartSlice";
 import { selectAuth } from "../../redux/selectors";
-import { selectCart } from "../../redux/selectors";
-// import { useMemo } from "react";
+// import { selectCart } from "../../redux/selectors";
+import { useMemo } from "react";
 import food from "../../images/food.jpg";
 
 export const GoodsItem = ({ name, price, id }) => {
- 
   const LOCALSTORAGE_KEY = id;
   let quantity = 0;
-  // const LOCALSTORAGE_KEY = "infoProduct2";
-  // const { products } = useSelector(selectCart);
-   
-   
-  
+
   const { goods } = useSelector(selectAuth);
   const dispatch = useDispatch();
-  
-  const addDish = (id) => {
 
-    
+  const addDish = (id) => {
     localStorage.setItem(
       LOCALSTORAGE_KEY,
       JSON.stringify({
         id,
-        quantity: quantity+=1,
+        quantity: (quantity += 1),
+       sumPrice : quantity*price
       })
     );
-    // const currentQuantity = localStorage.getItem(LOCALSTORAGE_KEY)
-    //  let parsedQuantity = JSON.parse(currentQuantity)
-  
- 
-  
-
-      
-    
 
     const preciseGood = goods.find((item) => item.id === id);
-    
 
-    
     dispatch(addGood(preciseGood));
   };
   return (
     <>
-      <img className={css.goodImage} src={food} alt="food" width="200" height="120" />
+      <img
+        className={css.goodImage}
+        src={food}
+        alt="food"
+        width="200"
+        height="120"
+      />
       <div className={css.descBox}>
-          <div className={css.infoGood}>  
-        <p className={css.descName}> {name}</p>
+        <div className={css.infoGood}>
+          <p className={css.descName}> {name}</p>
           <p className={css.descPrice}>{price}</p>
-          </div>
+        </div>
         <button className={css.descButton} onClick={() => addDish(id)}>
           Add
         </button>
